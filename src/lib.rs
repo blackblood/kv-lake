7bace7a91d8b9bc66c212cfd34a54068346829ba
Akshay Takkar <akshayt@m3india.in>
2020-12-16 23:41:46 +0530

    refactored lru logic lib code

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,202 @@
 use std::collections::HashMap;
-use std::collections::VecDeque;
+use std::sync::{ Arc, RwLock };
 
-pub struct LRUCache<T> {
-  pub map: HashMap<String, T>,
-  que: VecDeque<String>,
+pub struct Node<T: std::fmt::Display + std::clone::Clone> {
+    key: String,
+    value: T,
+    prev: Option<Arc<RwLock<Node<T>>>>,
+    next: Option<Arc<RwLock<Node<T>>>>
+}
+
+impl<T: std::fmt::Display + std::clone::Clone> Node<T> {
+    pub fn new(k: String, v: T) -> Node<T> {
+        Node { key: k, value: v, prev: None, next: None }
+    }
+    fn set_next(&mut self, n: Option<&Arc<RwLock<Node<T>>>>) {
+        if let Some(u_n) = n {
+            self.next = Some(Arc::clone(u_n));
+        } else {
+            self.next = None;
+        }
+    }
+
+    fn set_prev(&mut self, n: Option<&Arc<RwLock<Node<T>>>>) {
+        if let Some(n_n) = n {
+            self.prev = Some(Arc::clone(n_n));
+        } else {
+            self.prev = None;
+        }
+    }
+}
+
+impl<T: std::fmt::Display + std::clone::Clone> Drop for Node<T> {
+    fn drop(&mut self) {
+        println!("Dropping node: {}", self.value);
+    }
+}
+
+struct List<T: std::fmt::Display + std::clone::Clone> {
+    head: Option<Arc<RwLock<Node<T>>>>,
+    end: Option<Arc<RwLock<Node<T>>>>,
+    size: u8
+}
+
+struct ListIterator<T: std::fmt::Display + std::clone::Clone> {
+    ptr: Option<Arc<RwLock<Node<T>>>>
+}
+
+impl<T: std::fmt::Display + std::clone::Clone> Iterator for ListIterator<T> {
+    type Item = Arc<RwLock<Node<T>>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(curr_ptr) = self.ptr.take() {
+            if let Some(right_ptr) = curr_ptr.read().unwrap().next.as_ref() {
+                self.ptr = Some(Arc::clone(right_ptr));
+            }
+            Some(curr_ptr)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: std::fmt::Display + std::clone::Clone> List<T> {
+    fn new() -> List<T> {
+        List { head: None, end: None, size: 0 }
+    }
+
+    fn iter(&mut self) -> ListIterator<T> {
+        if let Some(h) = self.head.take() {
+            let itr = ListIterator { ptr: Some(Arc::clone(&h)) };
+            self.head = Some(h);
+            return itr;
+        } else {
+            return ListIterator { ptr: None }
+        }
+    }
+
+    fn is_empty(&mut self) -> bool {
+        if self.head.is_none() && self.end.is_none() {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn append(&mut self, n: Arc<RwLock<Node<T>>>) {
+        if self.is_empty() {
+            self.head = Some(Arc::clone(&n));
+            self.end = Some(Arc::clone(&n));
+        } else {
+            if let Some(e) = self.end.take() {
+                let mut n_lock = n.write().unwrap();
+                n_lock.prev = Some(Arc::clone(&e));
+                let mut e_lock = e.write().unwrap();
+                e_lock.next = Some(Arc::clone(&n));
+                self.end = Some(Arc::clone(&n));
+            } else {
+                panic!("List is in illegal state");
+            }
+        }
+        self.size += 1;
+    }
+
+    fn move_to_front(&mut self, n: Arc<RwLock<Node<T>>>) {
+        if let Some(h) = self.head.take() {
+            if let Some(n_prv) = n.read().unwrap().prev.as_deref() {
+                n_prv.write().unwrap().set_next(n.read().unwrap().next.as_ref());
+            } else {
+                self.head = Some(h);
+                return ();
+            }
+            if let Some(n_nxt) = n.read().unwrap().next.as_deref() {
+                n_nxt.write().unwrap().set_prev(n.read().unwrap().prev.as_ref());
+            } else {
+                if let Some(end) = n.read().unwrap().prev.as_ref() {
+                    self.end = Some(Arc::clone(end));
+                }
+            }
+            {
+                let mut m = n.write().unwrap();
+                m.set_next(Some(&h));
+            }
+            {
+                let mut m = n.write().unwrap();
+                m.set_prev(None);
+            }
+            {
+                let mut m = h.write().unwrap();
+                m.set_prev(Some(&n));
+            }
+            self.head = Some(n);
+        }
+    }
+
+    fn remove_last_node(&mut self) -> Option<Arc<RwLock<Node<T>>>> {
+        if let Some(e) = self.end.take() {
+            self.end = Some(Arc::clone(e.read().unwrap().prev.as_ref().unwrap()));
+            self.size -= 1;
+            Some(Arc::clone(&e))
+        } else {
+            None
+        }
+    }
+}
+
+pub struct LRUCache<T: std::fmt::Display + std::clone::Clone> {
+  pub map: HashMap<String, Arc<RwLock<Node<T>>>>,
+  list: List<T>,
   max_length: u8
 }
 
-fn index_of(key: &String, que: &VecDeque<String>) -> Option<usize> {
-  for (i, el) in que.iter().enumerate() {
-    if key.as_str() == el {
-      return Some(i);
-    }
-  }
-  return None;
-}
-
-impl<T> LRUCache<T> {
+impl<T: std::fmt::Display + std::clone::Clone> LRUCache<T> {
   pub fn new() -> LRUCache<T> {
-    LRUCache { map: HashMap::new(), que: VecDeque::new(), max_length: 3 }
+    LRUCache { map: HashMap::new(), list: List::new(), max_length: 3 }
   }
 
   pub fn put(&mut self, key: String, value: T) {
-    self.map.insert(key.clone(), value);
-    if let Some(del_index) = index_of(&key, &self.que) {
-      self.que.remove(del_index);
-    }
-    self.que.push_front(key.clone());
-    if (self.que.len() as u8) > self.max_length {
-      if let Some(last_el) = self.que.pop_back() {
-        self.map.remove(&last_el);
+      if !self.map.contains_key(&key) {
+          let new_node = Arc::new(RwLock::new(Node::new(key.clone(), value)));
+          if self.list.size >= self.max_length {
+              if let Some(rn) = self.list.remove_last_node() {
+                  self.map.remove(&rn.read().unwrap().key);
+              }
+          }
+          self.list.append(Arc::clone(&new_node));
+          self.map.insert(key.clone(), Arc::clone(&new_node));
       }
+  }
+
+  pub fn get(&mut self, key: String) -> Option<T> {
+    if let Some(node) = self.map.get(&key) {
+        self.list.move_to_front(Arc::clone(node));
+        return Some(node.read().unwrap().value.clone());
+    } else {
+        return None;
     }
   }
 
-  pub fn get(&mut self, key: String) -> Option<&T> {
-    if let Some(del_index) = index_of(&key, &self.que) {
-      self.que.remove(del_index);
-      self.que.push_front(key.clone());
-    }
-    self.map.get(&key)
+  pub fn print_list(&mut self) {
+      for n in self.list.iter() {
+          println!("{}", n.read().unwrap().value);
+      }
+  }
+
+  pub fn print_map(&self) {
+      for (k, v) in &self.map {
+          println!("{}: {}", k, v.read().unwrap().value);
+      }
   }
 }
+
+// fn main() {
+//     let mut cache = LRUCache::new();
+//     cache.put("first_name".to_string(), "Akshay".to_string());
+//     cache.put("middle_name".to_string(), "Vijay".to_string());
+//     cache.put("last_name".to_string(), "Takkar".to_string());
+//     cache.put("age".to_string(), 28.to_string());
+//     cache.put("gender".to_string(), "male".to_string());
+//     println!("\n");
+//     cache.print_list();
+//     cache.print_map();
+// }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::io;
 use std::io::Read;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::thread;
 
-enum Command {
-    PUT(String, String),
+enum Command<T> {
+    PUT(String, T),
     GET(String),
     QUIT,
 }
 
-fn get_command(raw_input: &str) -> Result<Command, String> {
+fn get_command(raw_input: &str) -> Result<Command<String>, String> {
+    let mut raw_input = raw_input.to_string();
+    raw_input.pop(); // removing trailing newline char
     let input_vec: Vec<&str> = raw_input.split(" ").collect();
     let c = input_vec[0];
     if c == "PUT" {
@@ -29,54 +31,54 @@ fn get_command(raw_input: &str) -> Result<Command, String> {
     }
 }
 
-fn handle_connection(conn: &mut TcpStream, cache: &mut Arc<Mutex<LRUCache<String>>>) -> io::Result<(String)> {
-    let mut input_length = [0; 1];
-    conn.read(&mut input_length)?;
-    let mut input_buffer: Vec<u8> = vec![0; input_length[0] as usize];
-    conn.read(&mut input_buffer)?;
-    let string_buffer = std::str::from_utf8(&input_buffer).unwrap();
-    println!("Received Command: {}", string_buffer);
-    match get_command(string_buffer) {
-        Ok(command) => match command {
-            Command::PUT(key, value) => {
-                cache.lock().unwrap().put(key.to_string(), value.to_string());
-                println!("Added {}: {}", key, value);
-                return Ok(format!("Added {}: {}", key, value));
-            }
-            Command::GET(key) => {
-                if let Some(res) = cache.lock().unwrap().get(key) {
-                    return Ok(res.to_string());
+fn handle_connection(conn: &mut TcpStream, cache: Arc<RwLock<LRUCache<String>>>) -> io::Result<String> {
+    loop {
+        let mut input_length = [0; 1];
+        conn.read(&mut input_length)?;
+        let mut input_buffer: Vec<u8> = vec![0; input_length[0] as usize];
+        conn.read(&mut input_buffer)?;
+        let string_buffer = std::str::from_utf8(&input_buffer).unwrap();
+        println!("Received Command: {}", string_buffer);
+        match get_command(string_buffer) {
+            Ok(command) => match command {
+                Command::PUT(key, value) => {
+                    let mut m_cache = cache.write().unwrap();
+                    m_cache.put(key.to_string(), String::clone(&value));
+                    m_cache.print_map();
+                    let output = format!("Added {}: {}", key, value);
+                    conn.write(&[output.len() as u8]).expect("len socket write failed");
+                    conn.write(output.as_bytes()).expect("data socket write failed");
                 }
-            }
-            Command::QUIT => {
-                return Ok(format!("quit ack"));
-            }
-        },
-        Err(_e) => (),
-    };
-    Ok("".to_string())
+                Command::GET(key) => {
+                    let mut m_cache = cache.write().unwrap();
+                    if let Some(output) = m_cache.get(key) {
+                        println!("output = {}", output);
+                        conn.write(&[output.len() as u8]).expect("len socket write failed");
+                        conn.write(output.as_bytes()).expect("data socket write failed");
+                    } else {
+                        println!("Not found in cache");
+                    }
+                    m_cache.print_list();
+                }
+                Command::QUIT => {
+                    println!("quit ack");
+                    return Ok("".to_string());
+                }
+            },
+            Err(_e) => (),
+        };
+    }
 }
 
 fn main() -> io::Result<()> {
-    let lru_cache_ptr = Arc::new(Mutex::new(LRUCache::new()));
+    let lru_cache_ptr = Arc::new(RwLock::new(LRUCache::new()));
     let conn = TcpListener::bind("localhost:8000")?;
     println!("Listening on port 8000");
     for stream in conn.incoming() {
         let mut sock = stream.unwrap().try_clone().unwrap();
-        let mut lru_cache = Arc::clone(&lru_cache_ptr);
+        let lru_cache_ref = Arc::clone(&lru_cache_ptr);
         thread::spawn(move || {
-            loop {
-                let output = handle_connection(&mut sock, &mut lru_cache).expect("Handle connection failed.");
-                println!("output = {}", output);
-                sock.write(&[output.len() as u8]).expect("len socket write failed");
-                sock.write(output.as_bytes()).expect("data socket write failed");
-                for (k, v) in lru_cache.lock().unwrap().map.iter() {
-                    println!("{}: {}", k, v);
-                }
-                if output == "quit ack" {
-                    break;
-                }
-            }
+            handle_connection(&mut sock, lru_cache_ref).expect("Handle connection failed.");
         });
     }
     Ok(())
